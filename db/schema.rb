088948c27b66bb9a46eeefb8fa8bc39175d0e575
Serena <serenafgreen@gmail.com>
2024-03-01 20:16:17 -0800

    adds pantry_item model to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_02_041145) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_02_041352) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pantry_items", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "ingredient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
