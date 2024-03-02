0279b8c2ac8c7d3d80c108a3fca103385ea7f61b
Serena <serenafgreen@gmail.com>
2024-03-01 20:26:41 -0800

    adds ingredient_recipe model to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_02_041352) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_02_042453) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredient_recipes", force: :cascade do |t|
+    t.integer "ingredient_id"
+    t.integer "recipe_id"
+    t.string "measurements"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
