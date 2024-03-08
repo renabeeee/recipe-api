170c3d91924e40c1a47253f8f10005c289661157
Serena <serenafgreen@gmail.com>
2024-03-07 20:53:34 -0800

    creates controller for ingredient_recipe model

--- app/controllers/pantry_items_controller.rb
+++ app/controllers/pantry_items_controller.rb
@@ -1,2 +1,15 @@
 class PantryItemsController < ApplicationController
+  def create
+    # if current_user.admin
+      @pantry_item = Pantry_item.new(
+        user_id: params[:name],
+        ingredient_id: params[:name],
+    )
+    if @pantry_item.save #happy path
+      render json: { message: "Your pantry item was saved." }
+      # render :show
+    else #sad path
+      render json: { errors: @pantry_item.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
 end
