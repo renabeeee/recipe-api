d5c2cb17a56b3c2428620edb3b18864e21b0c994
Serena <serenafgreen@gmail.com>
2024-03-07 19:20:52 -0800

    adds image column to recipes model

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,2 +1,21 @@
 class RecipeController < ApplicationController
+  def create
+    if current_user
+      @recipe = recipe.new(
+      user_id: current_user.id,
+      title: params[:title],
+      )
+      # @recipe.image = params[:image_url]
+
+    if @recipe.save #happy path
+      Rsvp.create(user: current_user, recipe: @recipe)
+      current_user.update(status: "recipe Leader")
+      render :show
+    else #sad path
+      render json: { errors: @recipe.errors.full_messages}, status: :unprocessable_entity
+    end
+    else
+      render json: { message: "Please login." }, status: :unauthorized
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_02_042453) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_08_031857) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_02_042453) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image"
+  end
+
+  create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
