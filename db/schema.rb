8bb911bfde25c5041861b5f8346a056ef81fabd7
Serena <serenafgreen@gmail.com>
2024-03-07 20:32:38 -0800

    removes password column from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_08_031857) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_08_042949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,7 +50,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_08_031857) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "email"
-    t.string "password"
     t.string "password_digest"
     t.string "profile_image"
     t.datetime "created_at", null: false
