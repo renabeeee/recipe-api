fd8fde816d16ae2d261e10713bc31c9288379744
Serena <serenafgreen@gmail.com>
2024-03-09 20:33:56 -0800

    creates method to add ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,12 @@
 class IngredientsController < ApplicationController
+  def create
+    @ingredient = Ingredient.new(
+        name: params[:name],
+      )
+        if @ingredient.save
+          render json: { message: "#{@ingredient.name} was added!" }
+        else
+          render json: { errors: @ingredient.errors.full_messages }, status: :bad_request
+        end
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   post "/recipes" => "recipes#create"
   post "/users" => "users#create"
   patch "/users/:id" => "users#update"
+  post "/ingredients" => "ingredients#create"
 
 end
