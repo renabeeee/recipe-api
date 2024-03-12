7a9acfa0c80e07071596247c6f6da5b99d3dc986
Serena <serenafgreen@gmail.com>
2024-03-11 22:52:35 -0700

    adds delete method + route for users to remove an ingredient from their pantry

--- app/controllers/pantry_items_controller.rb
+++ app/controllers/pantry_items_controller.rb
@@ -19,4 +19,15 @@ class PantryItemsController < ApplicationController
       render json: { errors: [@pantry_item.errors, "This is already in your pantry."], status: :unprocessable_entity }
     end
   end
+
+  def destroy
+    @pantry_item = PantryItem.find(params[:id])
+
+    if @pantry_item.user == current_user
+      @pantry_item.destroy
+      render json: { message: "Item removed from pantry." }
+    else
+      render json: { error: "You are not authorized to delete this item." }, status: :unauthorized
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get "/full-pantry" => "pantry_items#index"
   post "/sessions" => "sessions#create"
   post "/add-to-pantry" => "pantry_items#create"
+  delete "/remove-from-pantry" => "pantry_items#destroy"
 end
