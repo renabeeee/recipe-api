448b7c65805980e04d7d798dcf93804ef0f7f48a
Serena <serenafgreen@gmail.com>
2024-03-11 22:46:06 -0700

    adds create method + route for users to save an ingredient to their pantry

--- app/controllers/pantry_items_controller.rb
+++ app/controllers/pantry_items_controller.rb
@@ -5,4 +5,18 @@ class PantryItemsController < ApplicationController
     @pantry_items = current_user.pantry_items
     render :index
    end
+
+   def create
+    @ingredient = Ingredient.find(params[:ingredient_id])
+
+    @pantry_item = PantryItem.new(
+      user_id: current_user.id,
+      ingredient_id: @ingredient.id,
+      )
+    if @pantry_item.save
+      render json: { message: "Your item was saved! " }
+      else
+      render json: { errors: [@pantry_item.errors, "This is already in your pantry."], status: :unprocessable_entity }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   post "/ingredients" => "ingredients#create"
   patch "/ingredients/:id" => "ingredients#update"
   get "/full-pantry" => "pantry_items#index"
-
   post "/sessions" => "sessions#create"
-
+  post "/add-to-pantry" => "pantry_items#create"
 end
